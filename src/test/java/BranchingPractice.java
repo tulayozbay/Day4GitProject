4b9afe18008a194c2ce5f236e3b774e134ecb743
tulayozbay <82976910+tulayozbay@users.noreply.github.com>
2021-05-25 19:23:44 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,6 +2,13 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        //created new project
+        //enable version control
+        //added .gitignore with .idea folder and *.iml file
+        //made initial commit keeping track of all files
+        //shared project on github
+
+
     }
 
 
